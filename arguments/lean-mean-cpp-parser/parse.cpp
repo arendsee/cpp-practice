0600dc653bb4e71b16ef8495234a8c417748ebcf
Zebulun Arendsee <arendsee@iastate.edu>
2016-09-26 10:54:55 -0500

    Reduced boilerplate in subcommands
    My approach is not pretty. I used macros for direct code replacement.
    This works reasonably well since the subcommands contain a great deal of
    exactly replicated code. However, this is not a longterm solution, since
    surely I will find configurations where this breaks. An
    inheritance-based solution would be better, but there are some oddities
    about the library that confuse this approach.

--- arguments/lean-mean-cpp-parser/parse.cpp
+++ arguments/lean-mean-cpp-parser/parse.cpp
@@ -9,6 +9,26 @@ using std::string;
 
 #include "optionparser.h"
 
+#define SHIFT argc -= 1; argv += 1;
+
+#define BOILERPLATE                                                      \
+    if (argc == 0) {                                                     \
+        option::printUsage(cout, usage);                                 \
+        exit(EXIT_FAILURE);                                              \
+    }                                                                    \
+                                                                         \
+    option::Stats  stats(usage, argc, argv);                             \
+    option::Option options[stats.options_max], buffer[stats.buffer_max]; \
+    option::Parser parse(usage, argc, argv, options, buffer);            \
+                                                                         \
+    if (parse.error())                                                   \
+        exit(EXIT_FAILURE);                                              \
+                                                                         \
+    if (options[HELP]) {                                                 \
+        option::printUsage(cout, usage);                                 \
+        exit(EXIT_SUCCESS);                                              \
+    }                                                                    \
+
 void subcommand_foo(int argc, char* argv[])
 {
     enum  optionIndex { UNKNOWN, HELP, VERSION };
@@ -30,25 +50,7 @@ void subcommand_foo(int argc, char* argv[])
         {0,0,0,0,0,0}
     };
 
-    // If no arguments given, print help
-    if (argc == 0) {
-        option::printUsage(cout, usage);
-        exit(EXIT_FAILURE);
-    }
-
-    // Prepare parsing of base (help, version, or subcommand)
-    // This considers only the first argument
-    option::Stats  stats(usage, argc, argv);
-    option::Option options[stats.options_max], buffer[stats.buffer_max];
-    option::Parser parse(usage, argc, argv, options, buffer);
-
-    if (parse.error())
-        exit(EXIT_FAILURE);
-
-    if (options[HELP]) {
-        option::printUsage(cout, usage);
-        exit(EXIT_SUCCESS);
-    }
+    BOILERPLATE
 
     cout << "Doing foo\n";
 }
@@ -77,25 +79,7 @@ void subcommand_bar(int argc, char* argv[])
         {0,0,0,0,0,0}
     };
 
-    // If no arguments given, print help
-    if (argc == 0) {
-        option::printUsage(cout, usage);
-        exit(EXIT_FAILURE);
-    }
-
-    // Prepare parsing of base (help, version, or subcommand)
-    // This considers only the first argument
-    option::Stats  stats(usage, argc, argv);
-    option::Option options[stats.options_max], buffer[stats.buffer_max];
-    option::Parser parse(usage, argc, argv, options, buffer);
-
-    if (parse.error())
-        exit(EXIT_FAILURE);
-
-    if (options[HELP]) {
-        option::printUsage(cout, usage);
-        exit(EXIT_SUCCESS);
-    }
+    BOILERPLATE
 
     if(options[SCREAM]){
         cout << "Ahhhhahhahhh\n";
@@ -103,11 +87,12 @@ void subcommand_bar(int argc, char* argv[])
         cout << "Doing bar\n";
     }
 
-    exit(EXIT_SUCCESS);
+    cout << "Doing bar\n";
 }
 
 int main(int argc, char** argv)
 {
+    SHIFT
 
     enum  optionIndex { UNKNOWN, HELP, VERSION };
     const option::Descriptor usage[] = {
@@ -139,44 +124,28 @@ int main(int argc, char** argv)
         {0,0,0,0,0,0}
     };
 
-    // If no arguments given, print help
-    if (argc == 1) {
-        option::printUsage(cout, usage);
-        exit(EXIT_FAILURE);
-    }
-
-    // Prepare parsing of base (help, version, or subcommand)
-    // This considers only the first argument
-    option::Stats  stats(usage, 1, argv+1);
-    option::Option options[stats.options_max], buffer[stats.buffer_max];
-    option::Parser parse(usage, 1, argv+1, options, buffer);
-
-    if (parse.error())
-        exit(EXIT_FAILURE);
-
-    if (options[HELP]) {
-        option::printUsage(cout, usage);
-        exit(EXIT_SUCCESS);
-    }
+    BOILERPLATE
 
     if (options[VERSION]) {
         cout << "Version 0\n";
         exit(EXIT_SUCCESS);
     }
 
+    SHIFT 
+
     string subcommand(parse.nonOption(0));
 
-
     if(subcommand == "foo"){
-        subcommand_foo(argc - 2, argv + 2);
+        subcommand_foo(argc, argv);
     }
-
     else if(subcommand == "bar"){
-        subcommand_bar(argc - 2, argv + 2);
+        subcommand_bar(argc, argv);
     }
-
-    else{
-        cerr << "Failed to parse arguments\n\n";
+    else {
+        cerr << "Failed to parse arguments, subcommand '" << parse.nonOption(0) << "' is not defined\n\n";
         option::printUsage(cout, usage);
     }
+
 }
+
+#undef BOILERPLATE
